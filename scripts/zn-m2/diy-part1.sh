79db8bc2980135e9cb7782b06002590a4435b7cc
lbf1120247600-jpg <lbf1120247600@gmail.com>
2026-09-22 18:49:35 +0800

    zn-m2-lite: update scripts/zn-m2/diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+# zn-m2-lite DIY part 1 — feeds 加载前
+# 默认 IP 改为 192.168.1.1（保持与你当前路由一致）
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate 2>/dev/null || true
